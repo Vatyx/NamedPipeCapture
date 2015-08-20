a7e33a61a7237abf707a1cbdd4fbae859dc177f3
smueller <smueller@osisoft.com>
2015-08-20 16:49:29 -0700

    Added memcpy safety in the GetQueuedCompletionStatus handler. If memory is invalid that we're trying to read, the exception will be swallowed, and the application will move on as if there is no problem.

--- NamedPipeCapture/Globals.cpp
+++ NamedPipeCapture/Globals.cpp
@@ -4,17 +4,84 @@
 #include "NamedPipe.h"
 #include "NamedPipeServer.h"
 
-std::atomic<globalStruct*> globalStruct::globalObject = nullptr;
+std::shared_ptr<globalStruct> globalStruct::globalObject = nullptr;
 
-globalStruct* globalStruct::GetGlobals()
+std::shared_ptr<globalStruct> globalStruct::GetGlobals()
 {
-   return globalStruct::globalObject.load();
+   return std::atomic_load(&globalObject);
 }
 
-bool globalStruct::InitGlobals(globalStruct* obj)
+bool globalStruct::InitGlobals(const std::shared_ptr<globalStruct>& obj)
 {
-   globalStruct* pVal = nullptr;
-   return globalObject.compare_exchange_strong(pVal, obj);
+   std::shared_ptr<globalStruct> pVal;
+   return std::atomic_compare_exchange_strong(&globalObject, &pVal, obj);
+}
+
+std::shared_ptr<globalStruct> globalStruct::nullifyGlobals()
+{
+   auto retval = GetGlobals();
+   if (!retval)
+      return retval;
+   std::shared_ptr<globalStruct> resetVal;
+   std::atomic_store(&globalObject, resetVal);
+   return retval;
+}
+
+void globalStruct::ClearReadCollection()
+{
+   std::lock_guard<std::mutex> lg(waitingMutex);
+   waitingData.clear();
+}
+
+bool globalStruct::FindAndEraseReadData(LPOVERLAPPED lpOverlapped, std::pair<LPOVERLAPPED, LPVOID>& procData)
+{
+   std::lock_guard<std::mutex> lg(waitingMutex);
+   auto currentData = std::find_if(
+      GetGlobals()->waitingData.begin(), GetGlobals()->waitingData.end(),
+      [&lpOverlapped](const std::pair<LPOVERLAPPED, LPVOID>& element) -> bool
+   {
+      return element.first == lpOverlapped;
+   });
+
+   if (currentData == GetGlobals()->waitingData.end())
+   {
+      return false;
+   }
+   procData = std::move(*currentData);
+   waitingData.erase(currentData);
+   return true;
+}
+
+std::shared_ptr<StreamerTools::Streamer> globalStruct::GetStreamer()
+{
+   return std::atomic_load(&streamer);
+}
+
+std::shared_ptr<boost::asio::io_service> globalStruct::GetService()
+{
+   return std::atomic_load(&service);
+}
+
+std::shared_ptr<NamedPipeServer> globalStruct::GetServer()
+{
+   return std::atomic_load(&server);
+}
+
+std::shared_ptr<NamedPipe> globalStruct::GetPipe()
+{
+   return std::atomic_load(&pipe);
+}
+
+void globalStruct::ResetAllPointers()
+{
+   std::shared_ptr<StreamerTools::Streamer> resetStream;
+   std::atomic_store(&streamer, resetStream);
+   std::shared_ptr<NamedPipe> resetPipe;
+   std::atomic_store(&pipe, resetPipe);
+   std::shared_ptr<NamedPipeServer> resetServer;
+   std::atomic_store(&server, resetServer);
+   std::shared_ptr<boost::asio::io_service> resetSvc;
+   std::atomic_store(&service, resetSvc);
 }
 
 globalStruct::~globalStruct() {}
